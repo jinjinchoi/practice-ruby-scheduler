87a5d6bdff4ffaa5eccf65d84e8fb31f80796e23
JungIn Choi <luvimperfection@gmail.com>
2022-02-28 23:27:21 +0900

    일정 등록 로직에서 인풋 잘못 받고 있던 오류 수정

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -9,11 +9,17 @@ class ScheduleController < ApplicationController
   def addSchedule
     new_schedule = Schedule.new
     new_schedule.tutor_id = params[:tutor_id]
-    _start_date = DateTime.new(params["start_time(1i)"].to_i, params["start_time(2i)"].to_i, params["start_time(3i)"].to_i, params["start_time(4i)"].to_i, params["start_time(5i)"].to_i, 0, '+9')
-    new_schedule.start_date = _start_date
+    _start_date = DateTime.new(params["start_date(1i)"].to_i, params["start_date(2i)"].to_i, params["start_date(3i)"].to_i, params["start_time(4i)"].to_i, params["start_time(5i)"].to_i, 0, '+9')
+    puts(_start_date)
+    puts(_start_date.localtime)
+    new_schedule.start_date = _start_date.localtime
     new_schedule.active = 1
     if new_schedule.save
       redirect_to "/schedule/update/?tutor_id=#{params[:tutor_id]}&schedule_id=#{new_schedule.id}"
+    else
+      new_schedule.errors.full_messages.each do |message|
+        puts(message)
+      end
     end
   end
   def create
